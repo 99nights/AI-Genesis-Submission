3ca9266a85d933d0b81361ae10db648252a88a06
99nights <hefts-chaste3z@icloud.com>
2025-11-18 17:15:46 +0400

    updated Tailwind Import

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './tailwind.js';
 
 console.log('[index.tsx] Starting app initialization...');
 
